be6829349b8da73e22de82a6d44e4eda17f6b24d
DR-huzzy <drhuzzy1234@gmail.com>
2025-07-09 12:44:29 +0000

    websocket server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,50 @@
 //WebSocket server logic
+
+const WebSocket = require('ws');
+
+//create the websocket server
+const wss = new WebSocket.Server({ port: 8080});
+
+//Track all connected clients
+const clients = new Set();
+
+
+wss.on('connection', (ws) => {
+
+    //Add new client to the set
+    clients.add(ws);
+    console.log(`New client connected, Clients connected: ${clients.size}`);
+
+    // handle messages from client 
+    ws.on('message',(message) => {
+        // client message
+        console.log(`Recieved: ${message.toString()}`);
+
+        //broad cast to all other clients
+        //Ensures the client is still connected.
+        //excludes the sender from receiving their own message
+        clients.forEach((client) => {
+            if(client !== ws && client.readyState === WebSocket.OPEN){
+                client.send(message.toString());
+            }
+        })
+
+    })
+
+    // handle client disconnection
+    ws.on('close', () => {
+        clients.delete(ws);
+        console.log(`Clients disconnected. Remaining clients: ${clients.size}`);
+    })
+
+    // error handler
+    ws.on('error', (error) => {
+        console.log(`client error: ${error}`)
+    })
+
+    
+})
+
+console.log(`web socket server running on ws://localhost:8080`);
+
+
